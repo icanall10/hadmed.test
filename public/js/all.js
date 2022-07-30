3543e05702a46369842ee3aca511b7383da85ba6
Marat Darbishev <marat.icanall@ya.ru>
2022-07-30 03:49:46 +0300

    Fixes

--- public/js/all.js
+++ public/js/all.js
@@ -34,7 +34,16 @@
 
     function behaviors() {
 
+        $('.table')
+            .once('table', function () {
+                let $this = $(this);
 
+                $this
+                    .find('.tr')
+                    .css('grid-template-columns', $this.attr('data-columns'));
+
+                $this.show();
+            })
 
     }
 
