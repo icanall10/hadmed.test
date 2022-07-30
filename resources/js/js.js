bf26dcb9bcc4d2fdafbc265bc81e839d6feed733
Marat Darbishev <marat.icanall@ya.ru>
2022-07-30 07:23:51 +0300

    Fixes

--- resources/js/js.js
+++ resources/js/js.js
@@ -11,7 +11,31 @@
                     .css('grid-template-columns', $this.attr('data-columns'));
 
                 $this.show();
-            })
+            });
+
+
+        $('.tabs a')
+            .once('tabs')
+            .click(function (e) {
+                e.preventDefault();
+
+                let $this = $(this);
+
+                $this
+                    .closest('.tabs')
+                    .find('a')
+                    .each(function () {
+                        let item = $(this);
+
+                        item.removeClass('active');
+
+                        $('[data-tab-content="' + item.attr('data-tab') + '"]').removeClass('active');
+                    });
+
+                $this.addClass('active');
+
+                $('[data-tab-content="' + $this.attr('data-tab') + '"]').addClass('active');
+            });
 
     }
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -19,12 +19,14 @@ mix.styles([
     'resources/css/fonts.css',
     'resources/css/icons.css',
     'resources/vendor/jquery-ui-1.13.1.custom/jquery-ui.structure.min.css',
+    'resources/vendor/simplebar/simplebar.css',
     'resources/css/style.css'
 ], 'public/css/all.css');
 
 mix.scripts([
     'resources/vendor/jquery-3.5.1.min.js',
     'resources/vendor/jquery-ui-1.13.1.custom/jquery-ui.min.js',
+    'resources/vendor/simplebar/simplebar.min.js',
     'resources/js/helpers.js',
     'resources/js/js.js'
 ], 'public/js/all.js');
